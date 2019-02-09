5b769bd379a5b45b0b0b3c005a3222668cc3b639
Yousef Amar <yousefamar@gmail.com>
2019-02-09 00:36:15 +0000

    Revamp; create basics and publications

---
+++ data.js
@@ -0,0 +1,48 @@
+let publications = [
+	{
+		title: 'Zest: REST over ZeroMQ',
+		publisher: 'SPT-IoT 2019',
+		description: 'In this paper we introduce, Zest (REST over ZeroMQ), a middleware technology in support of an Internet of Things (IoT).',
+		pdf: 'res/papers/1570510166.pdf'
+	}, {
+		title: 'Providing Occupancy as a Service with Databox',
+		publisher: 'CitiFog 2018',
+		description: 'In this paper we present Occupancy-as-a-Service (OaaS) implemented as an app on Databox.',
+		pdf: 'res/papers/citifog18-final7.pdf'
+	}, {
+		title: 'An Information-Theoretic Approach to Time-Series Data Privacy',
+		publisher: 'W-P2DS 2018 / EuroDW 2018',
+		description: 'This paper presents a system for efficiently augmenting token-based access control with privacy-awareness without significantly impacting performance or utility.',
+		pdf: 'res/papers/a3-amar.pdf'
+	}, {
+		title: 'Building Accountability into the Internet of Things: The IoT Databox Model',
+		publisher: 'Journal of Reliable Intelligent Environments December 2017',
+		description: 'This paper outlines the IoT Databox model as a means of making the Internet of Things (IoT) accountable to individuals.',
+		pdf: 'res/papers/JREI-D-17-00019.pdf'
+	}, {
+		title: 'Balanced Message Distribution in Distributed Message Handling Systems',
+		publisher: 'US Patent (serial number: 15/794440) 2017',
+		description: 'This patent describes a combination of methodologies for arriving at near-optimal message distribution decisions in distributed messaging systems under specific constraints.',
+		pdf: 'javascript:alert("Coming soon!")'
+	}, {
+		title: 'Route-based Authorization and Discovery for Personal Data',
+		publisher: 'EuroDW 2017',
+		description: 'When faced with systems in which third party components need to advertise the availability of data they gather, while other such components need to access it, solutions for delegated authorisation and discovery APIs for interoperability are needed. This work explores possible solutions, and converges on a testable implementation.',
+		pdf: 'res/papers/eurodw17-final3.pdf'
+	}, {
+		title: 'Personal Data Management with the Databox: What\'s Inside the Box?',
+		publisher: 'CAN 2016',
+		description: 'A more detailed look at the Databox as it stood; a collection of physical and cloudhosted software components that provide for an individual data subject to manage, log and audit access to their data by other parties.',
+		pdf: 'res/papers/p49-mortier.pdf'
+	}, {
+		title: 'Privacy-Aware Infrastructure for Managing Personal Data',
+		publisher: 'SIGCOMM 2016',
+		description: 'A poster abstract giving an overview of Databox systems as they stood with a stronger focus on arbiter interactions.',
+		pdf: 'res/papers/p571-amar.pdf'
+	}, {
+		title: 'Incremental Dense Multi-modal 3D Scene Reconstruction',
+		publisher: 'IROS 2015',
+		description: 'In this paper, we propose a probabilistic model that efficiently exploits complementarity between different depth-sensing modalities for incremental dense scene reconstruction.',
+		pdf: 'res/papers/ondra2015iros.pdf'
+	}
+];
---
+++ index.js
@@ -0,0 +1,63 @@
+let listing = Vue.component('listing', {
+	props: [ 'title', 'subtitle', 'description', 'link' ],
+	template: '#listing'
+});
+
+new Vue({
+	el: '#header',
+	methods: {
+		toggleMenu(event) {
+			event.target.classList.toggle('active');
+			let elem = document.getElementsByTagName('nav')[0];
+			elem.style.maxWidth = elem.style.maxWidth ? null : window.innerWidth + 'px';
+		},
+		closeMenu(event) {
+			let navbar = document.getElementsByTagName('nav')[0];
+			if (navbar.style.maxWidth) {
+				document.getElementById('menu').classList.toggle('active');
+				navbar.style.maxWidth = null;
+			}
+		}
+	}
+});
+
+new Vue({
+	el: '#content',
+	components: {
+		listing: listing
+	},
+	data: {
+		input: '#hello',
+		publications: publications
+	},
+	created: function () {
+		// Fix annoying navbar stuck width bug
+		let navbar = document.getElementsByTagName('nav')[0];
+		window.addEventListener('resize', () => {
+			if (window.innerWidth >= 992)
+				navbar.style.maxWidth = '100%';
+			else {
+				navbar.style.maxWidth = null;
+				document.getElementById('menu').classList.remove('active');
+			}
+		});
+		window.addEventListener('scroll', () => {
+			let navLock = document.getElementById('navLockPosition');
+			if (window.scrollY >= navLock.offsetTop) {
+				navbar.parentElement.classList.add('fixed');
+				navbar.parentElement.parentElement.style.paddingBottom = '3em';
+			}
+			else {
+				navbar.parentElement.classList.remove('fixed');
+				navbar.parentElement.parentElement.style.paddingBottom = 0;
+			}
+		});
+	},
+	methods: {
+		toggleSection(event) {
+			event.target.classList.toggle('active');
+			let elem = event.target.parentElement.parentElement.lastElementChild;
+			elem.style.maxHeight = elem.style.maxHeight ? null : elem.scrollHeight + 'px';
+		}
+	}
+});
